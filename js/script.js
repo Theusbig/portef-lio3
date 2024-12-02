7ef3d854466aaab48b9a44f6654f851785d42dfb
mrliedson <alissonbrendo99@gmail.com>
2024-12-02 19:40:42 -0300

    final js2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 function toggleMode() {
-    const html = DocumentElement;
-    html.classlist.toggle("light");
+    const html = document.documentElement;
+    html.classList.toggle("light");
 
-const img = document.querySelector("#profile img");
-
-if(html.classlist.contains("light")){
-    img.setAttribute("src", "./img/avatar2.jpg");
-}else {
-    imh.setAttribute("src", "./img/avatar1.jfif");
-}
 
 
 }
